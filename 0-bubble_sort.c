3409a779674b98f3aa23cac6b513e2b0b4972bce
oscargomez87 <oscarstiven87@gmail.com>
2020-02-01 21:27:52 +0000

    Cleaner unsortable array check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,23 +11,22 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, sorted = 0, tsize = size;
 	int temp;
 
-	if (tsize > 1)
+	if (tsize < 2)
+		return;
+	while (!sorted)
 	{
-		while (!sorted)
+		sorted = 1;
+		for (i = 1; i <= tsize - 1; i++)
 		{
-			sorted = 1;
-			for (i = 1; i <= tsize - 1; i++)
+			if (array[i - 1] > array[i])
 			{
-				if (array[i - 1] > array[i])
-				{
-					temp = array[i - 1];
-					array[i - 1] = array[i];
-					array[i] = temp;
-					sorted = 0;
-					print_array(array, size);
-				}
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
+				sorted = 0;
+				print_array(array, size);
 			}
-			tsize -= 1;
 		}
+		tsize -= 1;
 	}
 }
